bbd679c571d66ea30a43624a54c22ff4e113371c
bayu setiawan <bayusetiawanipb@gmail.com>
2022-08-12 14:18:25 +0700

    colors, utilites, breakpoints, grid, functions, button, card, navbar created

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("by-ui/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("by-ui/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["by-ui/**/*.scss"], buildStyles);
+  watch(["by-ui/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
